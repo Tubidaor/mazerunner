a88a553ccd9800eb0e751b3cd37d66bf380ef73b
tubidaor <juan.baltazar1@gmail.com>
2020-11-26 10:24:48 -0800

    added vercel json

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App'
+import React from 'react'
+import { ReactDom } from 'react-dom'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const div = document.createElement('div')
+  ReactDom.render(<App></App>, div)
+  ReactDom.unmountComponentAtNode(div)
+})
